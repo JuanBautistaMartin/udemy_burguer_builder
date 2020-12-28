9cd1ef5da40e36b5a1dee0cbca2517ae65895598
juanBautista <juanbautista19@gmail.com>
2020-12-28 17:09:13 +0100

    BurguerBuilder component basic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout/Layout'
+import BurguerBuilder from './containers/BurguerBuilder/BurguerBuilder';
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <p>Test</p>
+          <BurguerBuilder />
         </Layout>
       </div>
     );
