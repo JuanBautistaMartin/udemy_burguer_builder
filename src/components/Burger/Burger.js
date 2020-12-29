d2313b2d26d79b53f0c710f9c9ec6eec177142d8
juanBautista <juanbautista19@gmail.com>
2020-12-29 10:08:32 +0100

    Adding ingredients dinamically in a basic way, show a message if there are no ingredients specified in Burger component

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,30 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredients/BurgerIngredient';
 
 const burger = (props) => {
+    console.log("Porps Ingredients", props.ingredients);
+
+    let transformedIngredients = 
+        Object.keys(props.ingredients)
+            .map(igKey => {
+                return [...Array(props.ingredients[igKey])]
+                    .map((_, i) => {
+                        return <BurgerIngredient key={igKey + i} type={igKey} />
+                    });
+            })
+            .reduce((arr, el) => {
+                return arr.concat(el);
+            }, []);
+
+    console.log('TransformedIngredients', transformedIngredients);
+
+    if(transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>
+    }
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
