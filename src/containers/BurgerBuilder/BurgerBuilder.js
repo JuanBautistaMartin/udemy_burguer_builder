db1e46024dbc698b2cb9a52b3045f331aa8facb6
juanBautista <juanbautista19@gmail.com>
2020-12-29 11:50:27 +0100

    Adding and removing ingredients dinamically by pressing More or Less buttons

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,12 @@ import Aux from '../../hoc/Auxiliar';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+const INGREDIENTS_PRICE = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+}
 class BurgerBuilder extends Component {
 
     state = {
@@ -11,16 +17,49 @@ class BurgerBuilder extends Component {
             bacon: 0,
             cheese: 0,
             meat: 0
-        }
+        },
+        totalPrice: 4
     }
 
     render () {
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients}/>
-                <BuildControls />
+                <BuildControls 
+                    ingredientAdded={this.addIngredientsHandler} 
+                    ingredientRemoved={this.removeIngre3dientHandler} />
             </Aux>
         );
+    }
+
+    addIngredientsHandler = (type) => {
+        const updatedIngredients = {
+            ...this.state.ingredients
+        }
+        updatedIngredients[type] = this.state.ingredients[type] + 1;
+
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + INGREDIENTS_PRICE[type];
+
+        this.setState({
+            ingredients: updatedIngredients,
+            totalPrice: newPrice
+        });
+    }
+
+    removeIngre3dientHandler = (type) => {
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = this.state.ingredients[type] -1;
+
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice - INGREDIENTS_PRICE[type];
+
+        this.setState({
+            ingredients: updatedIngredients,
+            totalPrice: newPrice
+        });
     }
 }
 
