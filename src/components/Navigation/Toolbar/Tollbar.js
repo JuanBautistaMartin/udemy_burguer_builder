2a3dfc56fc12f8529e5b9867f0f685c8044ed9a9
juanBautista <juanbautista19@gmail.com>
2020-12-30 15:52:54 +0100

    Adding basic Toolbar component

---
+++ src/components/Navigation/Toolbar/Tollbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './Tollbar.css'
+
+const tollbar = (props) => (
+    <header className={classes.Toolbar}>
+        <div>MENU</div>
+        <div>LOGO</div>
+        <nav>
+            ...
+        </nav>
+    </header>
+);
+
+export default tollbar;
